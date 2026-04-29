0cf733b28d00ed8a7a4d78296b6217524a2445c5
koderDev <sapkotaanuj65@gmail.com>
2026-04-29 13:54:26 +0545

    todo highlight yellow color add

--- extension.js
+++ extension.js
@@ -1,36 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "remind-me" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('remind-me.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from remind-me!');
-	});
-
-	context.subscriptions.push(disposable);
+function highlighttodo(editor,todoDecoration){
+    if(!editor)return
+    const ranges=[]
+    for(let i=0;i<editor.document.lineCount;i++){
+        const line=editor.document.lineAt(i).text;
+        const match=line.match(/\/\/\s*TODO.*/i);
+        if(match) {
+            const start=line.indexOf(match[0])
+            ranges.push(new vscode.Range(i,start,i,start+match[0].length))
+        }
+    }
+    editor.setDecorations(todoDecoration,ranges)
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
 
-module.exports = {
-	activate,
-	deactivate
+function activate(context){
+    console.log('IT WORKS')
+
+	const todoDecoration = vscode.window.createTextEditorDecorationType({
+		backgroundColor: 'rgba(255,200,0,0.3)',
+		color: '#000',
+		fontWeight: 'bold'
+	})
+    highlighttodo(vscode.window.activeTextEditor,todoDecoration)
+
+    vscode.window.onDidChangeActiveTextEditor(e=>highlighttodo(e,todoDecoration),null,context.subscriptions)
+    vscode.workspace.onDidChangeTextDocument(()=>highlighttodo(vscode.window.activeTextEditor,todoDecoration),null,context.subscriptions)
+
+	context.subscriptions.push(todoDecoration);
 }
+
+
+function deactivate() {
+}
+
+module.exports = {activate,deactivate};
